285394499e36d69f95c477d164575e43092e1618
AnkeetSah <ankeetkumar7777@gmail.com>
2025-09-19 00:38:38 +0530

    corrected the isloading issue in the landing gaurd

--- src/middleware/LandingGuard.jsx
+++ src/middleware/LandingGuard.jsx
@@ -20,7 +20,7 @@ const LandingGuard = ({ children }) => {
     checkAuth();
   }, []);
 
-if (isLoading)
+if (loading)
   return (
     <div className="flex items-center justify-center h-screen">
       <div className="flex flex-col items-center">
